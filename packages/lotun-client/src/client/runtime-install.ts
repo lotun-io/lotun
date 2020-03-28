4cc6fa73ae3cc58b71d983227d4cadb16ab8ddf6
Matus Sabo <lamanabie@gmail.com>
2020-03-28 21:22:35 +0100

    fix @lotun/client npm path

--- packages/lotun-client/src/client/runtime-install.ts
+++ packages/lotun-client/src/client/runtime-install.ts
@@ -2,22 +2,10 @@ import path from 'path';
 
 if (!process.env.LOTUN_USE_GLOBAL_NPM) {
   // set for global-npm package
-  process.env.GLOBAL_NPM_BIN = path.join(
-    __dirname,
-    '..',
-    '..',
-    'node_modules',
-    '.bin',
-    'npm',
-  );
+  const nodeModulesPath = path.resolve(require.resolve('npm'), '../../../');
 
-  process.env.GLOBAL_NPM_PATH = path.join(
-    __dirname,
-    '..',
-    '..',
-    'node_modules',
-    'npm',
-  );
+  process.env.GLOBAL_NPM_BIN = path.join(nodeModulesPath, '.bin', 'npm');
+  process.env.GLOBAL_NPM_PATH = path.join(nodeModulesPath, 'npm');
 }
 
 import { npmImportAsync } from 'runtime-npm-install';
